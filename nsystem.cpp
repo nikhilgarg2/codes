6d9080907441081e73de577dc5336e1910592d7f
Nikhil Garg <nikhilgarg2@gmail.com>
2014-07-15 22:40:45 +0530

    modified:   nsystem.cpp

--- nsystem.cpp
+++ nsystem.cpp
@@ -1,48 +1,93 @@
 #include<iostream>
 #include<cstdio>
+#include<cstring>
 using namespace std;
+
+char last(int a)
+{
+	if(a==1000)
+		return 'm';
+	else if(a==100)
+		return 'c';
+	else if(a==10)
+		return 'x';
+	else if(a==1) 
+		return 'i';
+}
+
+
 int num(char c)
 {
-if(c=='m')
-return 1000;
-else if(c=='c')
-return 100;
-else if(c=='x')
-return 10;
-else if(c=='i')
-return 1;
-
-
-
+	if(c=='m')
+		return 1000;
+	else if(c=='c')
+		return 100;
+	else if(c=='x')
+		return 10;
+	else if(c=='i')
+		return 1;
 }
+
+void encrypt(int a)
+{
+	int c;
+	for(int i=1000;i>0;i=i/10)
+	{
+		c=a/i;
+		if(c>0)
+		{
+			if(c==1)
+			{
+				cout<<last(i);
+				a=a%i;
+			}
+
+			else
+			{
+
+				cout<<c<<last(i);
+				a=a%i;
+			}
+
+		}
+
+
+	}
+	cout<<"\n";
+}
+
 
 int decrypt(char a[])
 {
+	int sum=0;
 	char c,d;
-int sum=0;
-c=gc();
-c=gc();
-cout<<c<<endl;
-	for(;(47<c && c<58) || (c=='m') || (c=='c')||(c=='x')||(c=='i') ;c=gc())
-	{
-	cout<<sum<<c<<endl;	
+	for(int i=0;i<strlen(a);i++)
+	{   	c=a[i];
 		if(c>57)
 			sum+=num(c);
 		else
+		{	
 			d=c;
-			c=gc();
+			i++;
+			c=a[i];
 			sum+=(d-48)*num(c);
-	
+
+		}
 	}
-return sum;
+	return sum;
 }
+
 
 int main()
 {
-char a[100];
-cin>>a;
-
-cout<<decrypt(a)<<endl;	
-
-return 0;
+	char a[100],b[100];
+	int c,d;
+	scanf("%d",&d);
+	while(d--)
+	{
+		scanf("%s %s",a,b);
+		c=decrypt(a)+decrypt(b);
+		encrypt(c);	
+	}
+	return 0;
 }
